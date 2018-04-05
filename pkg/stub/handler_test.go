f462f9f2add1ea3fcdc033dc8ca098d0b7a0779e
John McKenzie <jmckind@gmail.com>
2018-04-05 17:00:05 -0500

    Add a basic test

--- pkg/stub/handler_test.go
+++ pkg/stub/handler_test.go
@@ -13,3 +13,12 @@
 // limitations under the License.
 
 package stub
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+  assert.True(t, true, "True is true!")
+}
